556b3ebd3d43b87b56d31ac8b23e46595254869f
gregclar2019 <daddygreggy09@gmail.com>
2023-04-05 05:29:22 -0400

    'Access-Control-Allow-Credentials', 'true'

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(express.json())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', '*');
     res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader(
       "Access-Control-Allow-Methods",
       "OPTIONS, GET, POST, PUT, PATCH, DELETE"
